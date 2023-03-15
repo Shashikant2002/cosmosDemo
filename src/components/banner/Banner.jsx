3d783f00b43eb246467a84eba0eb0b3b9dff0268
Shashikant <shashikant384443>
2023-03-15 16:16:50 +0530

    ...

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,25 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "./banner.css";
-import { BsChevronDown } from 'react-icons/bs';
+import { BsChevronDown } from "react-icons/bs";
+import axios from "axios";
 
 const Banner = () => {
+  const baseUrl = process.env.PUBLIC_URL || "http://localhost:5000";
+  const [banner, setBanner] = useState();
+
+  const fetchBannerData = async () => {
+    try {
+      const data = await axios.get(`${baseUrl}/api/get/all/banners`);
+      setBanner(data.data);
+    } catch (err) {
+      console.log("Error " + err);
+    }
+  };
+
+  useEffect(() => {
+    fetchBannerData();
+  }, []);
+
+  console.log(banner && banner[0].image_url);
+
   return (
     <>
-        <section id='banner' className="banner">
-            <div className="container">
-                <div className="content text-center">
-                    <h1>Contemporary Japanese Cuisine</h1>
-                    <div className="buttons flex justify-center">
-                      <button className='filled-button'>Order Now</button>
-                      <button className='border-button'>View All Location</button>
-                    </div>
-                </div>
-
-                <a className='flex align-center scroll-content justify-center' href="#about"><BsChevronDown /> Scroll Now</a>
+      {banner ? (
+        <section
+          id="banner"
+          className="banner"
+          style={{
+            padding: " 230px 0",
+            width: "100%",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            position: "relative",
+            background: `linear-gradient(rgba(0,0,0,.7).image_url, rgba(0,0,0,.7)), url(${
+              banner && banner[0].image_url
+            })`,
+          }}
+        >
+          <div className="container">
+            <div className="content text-center">
+              <h1>Contemporary Japanese Cuisine</h1>
+              <div className="buttons flex justify-center">
+                <button className="filled-button">Order Now</button>
+                <button className="border-button">View All Location</button>
+              </div>
             </div>
-        </section>
-    </>
-  )
-}
 
-export default Banner
+            <a
+              className="flex align-center scroll-content justify-center"
+              href="#about"
+            >
+              <BsChevronDown /> Scroll Now
+            </a>
+          </div>
+        </section>
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
+
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import About from "../components/aboutUs/About";
 import Banner from "../components/banner/Banner";
-import Regervation from "../components/booksheet/Regervation";
-import Dinying from "../components/dinyingExp/Dinying";
+// import Regervation from "../components/booksheet/Regervation";
+// import Dinying from "../components/dinyingExp/Dinying";
 import OurMenues from "../components/menues/OurMenues";
 
 // For Change Title
 import { Helmet } from "react-helmet";
 
 const Home = () => {
+
   return (
     <>
-    <Helmet><title>Home</title></Helmet>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
       <Banner />
       <About />
-      {/* <OurMenues /> */}
+      <OurMenues />
       {/* <Dinying /> */}
       {/* <Regervation /> */}
     </>
