30027002026ba06eac3927b3ef227b22a86044a0
Shashikant <shashikant384443>
2023-05-04 12:16:09 +0530

    ...

--- src/components/productCategory/ProductCategory.jsx
+++ src/components/productCategory/ProductCategory.jsx
@@ -23,7 +23,7 @@ const ProductCategory = ({ categoryes }) => {
       items: 1,
     },
   };
-
+  console.log(categoryes?.all_category_for_filter);
   return (
     <>
       <div className="productCategory common-section">
@@ -31,17 +31,29 @@ const ProductCategory = ({ categoryes }) => {
           {categoryes ? (
             <Carousel infinite={true} responsive={responsive}>
               {categoryes &&
-                categoryes.categoryForFilter.map((ele, i) => {
+                // categoryes.categoryForFilter.map((ele, i) => {
+                categoryes?.all_category_for_filter.map((ele, i) => {
                   return (
-                    <Link to={`/category/${ele._id}`} key={i} className="card flex flex-column justify-center align-center">
+                    <Link
+                      to={`/category/${ele._id}`}
+                      key={i}
+                      className="card flex flex-column justify-center align-center"
+                    >
                       <figure className="catImage">
-                        <img
-                          src="https://firebasestorage.googleapis.com/v0/b/kozmo-cloud.appspot.com/o/kozmocloud%2Fproducts%2FChiken%20Tendor%2FWed%2C%2003%20May%202023%2006%3A21%3A01%20GMT--Chiken%20Tendor.webp?alt=media&token=0fd13362-3577-477e-8db8-0249af74dcf1"
-                          alt=""
-                        />
+                        {ele?.main_category_image?.image_url ? (
+                          <img
+                            src={ele?.main_category_image?.image_url}
+                            alt=""
+                          />
+                        ) : (
+                          <img
+                            src="https://firebasestorage.googleapis.com/v0/b/kozmo-cloud.appspot.com/o/kozmocloud%2Fproducts%2FChiken%20Tendor%2FWed%2C%2003%20May%202023%2006%3A21%3A01%20GMT--Chiken%20Tendor.webp?alt=media&token=0fd13362-3577-477e-8db8-0249af74dcf1"
+                            alt=""
+                          />
+                        )}
                       </figure>
                       <p>
-                        <b>{ele._id}</b>
+                        <b>{ele.main_category_name}</b>
                       </p>
                     </Link>
                   );
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -33,6 +33,8 @@ const Product = () => {
     setLoading(false);
   }, [render]);
 
+  // console.log(allProduct);
+
   return (
     <>
       {loading ? (
