3725ebca865f65024e95872c62907c04b2eea531
Shashikant <shashikantprajapti17@gmail.com>
2023-05-20 10:39:17 +0530

    ..

--- src/components/stickyFoot/InstaSticky.jsx
+++ src/components/stickyFoot/InstaSticky.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./insta.css";
+import { Link } from "react-router-dom";
 
 const InstaSticky = () => {
   return (
     <>
-      <a target="_blank" className="instaStick" href="https://instagram.com/kozmo.by.nua?igshid=NTc4MTIwNjQ2YQ=="><img src="/assets/img/instagram.png" alt="" /></a>
+      <div className="instaStick flex align-center justify-center">
+        <Link className="filled-button" to="/product">Order Now</Link>
+        <a
+          target="_blank"
+          className="insta"
+          href="https://instagram.com/kozmo.by.nua?igshid=NTc4MTIwNjQ2YQ=="
+        >
+          <img src="/assets/img/update_1/instagram icon.svg" alt="" />
+        </a>
+      </div>
     </>
   );
 };
